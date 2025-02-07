0f6a92e4aa4dafa2e2409f14d28d2ac779fb088d
Jung Eun Ji <ejjung312@naver.com>
2025-02-07 09:30:05 +0900

    모니터링 화면 UI 작업

--- ParkingSystem/ViewModels/ParkingViewModel.cs
+++ ParkingSystem/ViewModels/ParkingViewModel.cs
@@ -1,10 +1,19 @@
-﻿namespace ParkingSystem.ViewModels
+﻿using System.Collections.ObjectModel;
+
+namespace ParkingSystem.ViewModels
 {
     public class ParkingViewModel : ViewModelBase
     {
+        public ObservableCollection<string> ImageList { get; set; }
+
         public ParkingViewModel()
         {
-            
+            ImageList = new ObservableCollection<string>
+            {
+                "/Video/enter.jpg",
+                "/Video/enter.jpg",
+                "/Video/enter.jpg",
+            };
         }
 
         public override void Dispose()
