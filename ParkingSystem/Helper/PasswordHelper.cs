5305cbe56ac47313366263dc3c33cc57e1b65cfd
Jung Eun Ji <ejjung312@naver.com>
2025-02-12 11:37:05 +0900

    비밀번호 별표 처리

---
+++ ParkingSystem/Helper/PasswordHelper.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Helper
+{
+    public class PasswordHelper
+    {
+        public static readonly DependencyProperty BoundPasswordProperty =
+        DependencyProperty.RegisterAttached("BoundPassword", typeof(string), typeof(PasswordHelper),
+            new PropertyMetadata(string.Empty, OnBoundPasswordChanged));
+
+        public static string GetBoundPassword(DependencyObject obj)
+        {
+            return (string)obj.GetValue(BoundPasswordProperty);
+        }
+
+        public static void SetBoundPassword(DependencyObject obj, string value)
+        {
+            obj.SetValue(BoundPasswordProperty, value);
+        }
+
+        private static void OnBoundPasswordChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is PasswordBox passwordBox)
+            {
+                passwordBox.PasswordChanged -= PasswordBox_PasswordChanged;
+                passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
+            }
+        }
+
+        private static void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is PasswordBox passwordBox)
+            {
+                SetBoundPassword(passwordBox, passwordBox.Password);
+            }
+        }
+    }
+}
